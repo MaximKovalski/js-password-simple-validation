e6e68f107bd2021f08462716cd070633c41bf252
MaximKovalski <108683838+MaximKovalski@users.noreply.github.com>
2023-03-01 20:33:58 +0200

    Update password.js

--- password.js
+++ password.js
@@ -1,5 +1,7 @@
 let passwords = [12345, "test123", "TEST1234", 2234];
-let counter = 0;
+let counter = 5;
+let error = document.getElementById("error");
+
 function password(passwordId) {
   for (let i = 0; i < passwords.length; i++) {
     if (passwordId == passwords[i]) {
@@ -12,10 +14,10 @@ function checkPassword() {
   let passwordId = document.getElementById("passwordCheck").value;
   let passFun = password(passwordId);
   if (passwordId !== passwords) {
-    counter++;
+    counter--;
     if (passFun === true) {
       location.href = "https://google.com";
-    } else if (counter > 4) {
+    } else if (counter < 0) {
       document.querySelector("#btn").disabled = true;
       let para = document.createElement("p");
       let node = document.createTextNode("Your password is blocked");
@@ -23,8 +25,9 @@ function checkPassword() {
       let divElement = document.getElementById("para");
       para.appendChild(node);
       divElement.appendChild(para);
+      error.innerHTML = "";
     } else {
-      alert("Wrong Password, Try Again");
+      error.innerHTML = `you have ${counter} attempts`;
     }
   }
 }
